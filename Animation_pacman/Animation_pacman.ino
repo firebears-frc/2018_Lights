438d1e92f4c42ac8b758187125b0da827fd431e3
robot <robot@10.30.11.202>
2018-01-30 19:29:33 -0600

    hello

---
+++ Animation_pacman/Animation_pacman.ino
@@ -0,0 +1,24 @@
+#include <Adafruit_NeoPixel.h>
+#include <PixelStrip.h>
+#include <Animation.h>
+#include "PacmanAnimation.h"
+
+const int PIN = 7;
+const int NUM_PIXELS = 8;
+const int BRIGHTNESS = 67;
+
+PixelStrip *strip = new PixelStrip(NUM_PIXELS, PIN, NEO_GRB);
+PacmanAnimation *animation;
+
+void setup() {
+  animation = new PacmanAnimation();
+  strip->setup();
+  strip->setBrightness(BRIGHTNESS);
+  strip->setAnimation(animation);
+}
+
+void loop() {
+  strip->draw();
+  strip->show();
+  delay(10);
+}
